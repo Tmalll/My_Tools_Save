c106dcafc8d93808b2d7fee9e5f5b1d2cd065703
Tmalll <40192806+Tmalll@users.noreply.github.com>
2026-02-10 10:27:42 +0800

    update 2026_02_10_周二 10.27.41.88

--- 18_BraveBrowser_update.bat/update_brave_v1.bat
+++ 18_BraveBrowser_update.bat/update_brave_v1.bat
@@ -1,15 +1,13 @@
 @echo off
-echo 通用github下载脚本
-
 set "项目地址=/brave/brave-browser/releases/latest"
 set "API=https://api.github.com/repos%项目地址%"
-set "proxy=https://cfproxy.miaosky.top/proxy/"
+set "cfproxy=https://cfproxy.miaosky.top/proxy/"
 set "bestIP=2606:4700::fb27:dda9:fa5a"
 
-echo 项目地址为: [ %项目地址% ]
-echo API地址为: [ %API% ]
-echo 使用前置代理为: [ %proxy% ]
-echo 使用优选IP为: [ %bestIP% ]
+echo 项目地址为: [ %项目地址% ] && echo.
+echo API地址为: [ %API% ] && echo.
+echo 使用前置代理为: [ %cfproxy% ] && echo.
+echo 使用优选IP为: [ %bestIP% ] && echo.
 
 :: 获取包含 win32-x64.zip 的下载地址
 for /f "usebackq delims=" %%A in (`powershell -NoProfile -Command ^
@@ -22,30 +20,25 @@ for /f "usebackq delims=" %%A in (`powershell -NoProfile -Command ^
   set "dlurl=%%A"
 )
 
-echo.
 echo [OK] 找到下载地址:
 echo %dlurl%
 echo.
 
 :: 拼接代理 + 下载地址
-set "finalurl=%proxy%%dlurl%"
+set "finalurl=%cfproxy%%dlurl%"
 
-echo.
-echo 完整下载地址为: 
+echo [ 完整下载地址为 ]: 
 echo    %finalurl%
 echo.
 
-echo 测试下载地址
-curl -L "%finalurl%" --range 0-0 --resolve cfproxy.miaosky.top:443:%bestIP% ^
-        -o NUL --silent ^
-     -w "\n 相应的HTTP代码为: [ %%{http_code} ] \n\n 所请求的服务器IP地址为: [ %%{remote_ip} ]\n"
+echo [ 下载前清理 ]
+del /q brave-*-win32-x64.zip
 echo.
 
-echo. && echo 开始正式下载 && echo.
+echo [ 开始正式下载 ]
 curl -O -L "%finalurl%" --resolve cfproxy.miaosky.top:443:%bestIP%
 echo.
 
-echo.
 echo [OK] 下载完成.
 echo.
 
@@ -53,9 +46,16 @@ echo [ 解压安装包 ]
 powershell -Command "Get-ChildItem -Filter 'brave-*-win32-x64.zip' | ForEach-Object { Write-Host ''; Write-Host '开始解压:' $_.FullName; Expand-Archive -Path $_.FullName -DestinationPath . -Force }" && echo. && echo [ 解压成功完成 ] && echo.
 
 echo [ 删除多余安装包 ]
-timeout /t 3 >nul
-del /q brave-*-win32-x64.zip && echo [ 删除完成 ] && echo.
+del /q brave-*-win32-x64.zip
+echo.
 
 
 pause
 exit
+
+
+echo 测试下载地址
+curl -L "%finalurl%" --range 0-0 --resolve cfproxy.miaosky.top:443:%bestIP% ^
+        -o NUL --silent ^
+     -w "\n 相应的HTTP代码为: [ %%{http_code} ] \n\n 所请求的服务器IP地址为: [ %%{remote_ip} ]\n"
+echo.
