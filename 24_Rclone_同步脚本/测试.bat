9e2b5cc34e3b21d64e6169f37270aa0d88fcece4
Tmalll <40192806+Tmalll@users.noreply.github.com>
2026-07-22 18:40:46 +0800

    sync update 2026_07_22_周三 18.40.46.38

--- 24_Rclone_同步脚本/测试.bat
+++ 24_Rclone_同步脚本/测试.bat
@@ -1,5 +1,5 @@
 
-set "运行脚本=E:\01.userData\ZhuoMian\工具存档\24_rclone_windows_本地同步备份脚本\03_备份_Github仓库_.bat"
+set "运行脚本=E:\01.userData\ZhuoMian\工具存档\24_Rclone_同步脚本\04_备份_ZhuoMian_软件目录.bat"
 set "日志文件=E:\01.userData\ZhuoMian\Rclone.log"
 
 start /b /wait "" cmd /c ""%运行脚本%" > "%日志文件%" 2>&1"
